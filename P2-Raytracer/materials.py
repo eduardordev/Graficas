6a46f14990a1b21ae46db14ff0aa18a0a0c0fb39
Alejandro Puente <eduardorh1312@gmail.com>
2021-11-16 12:32:58 -0600

    update rt

--- P2-Raytracer/materials.py
+++ P2-Raytracer/materials.py
@@ -11,16 +11,21 @@ ORANGE = color(245,149,29)
 ORANGE1 = color(238,136,26)
 ORANGE2 = color(230,126,29)
 ORANGE3 = color(224,114,25)
-
-
+ORANGE4 = color(202,82,21)
+ORANGE5 = color(208,91,21)
+ORANGE6 = color(216,105,26)
+ORANGE7 = color(158,33,29)
 
 skin = Material(diffuse=SKIN, albedo=(0.6, 0.3, 0.1, 0), spec=50)
-skin1 = Material(diffuse=SKIN, albedo=(0.6, 0.3, 0.1, 0), spec=50)
+skin1 = Material(diffuse=SKIN1, albedo=(0.6, 0.3, 0.1, 0), spec=50)
 orange = Material(diffuse=ORANGE, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
 orange1 = Material(diffuse=ORANGE1, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
 orange2 = Material(diffuse=ORANGE2, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
 orange3 = Material(diffuse=ORANGE3, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
-
+orange4 = Material(diffuse=ORANGE4, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
+orange5 = Material(diffuse=ORANGE5, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
+orange6 = Material(diffuse=ORANGE6, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
+orange7 = Material(diffuse=ORANGE7, albedo=(0.9, 0.2, 0, 0.1, 0), spec=150)
 
 leaf = Material(diffuse=GREEN, albedo=(1, 1, 0, 0), spec=50, refractive_index=0)
 water = Material(diffuse=LIGHT_PURPLE, albedo=(0, 0.5, 0.1, 0.8), spec=125, refractive_index=1.5)
--- P2-Raytracer/run.py
+++ P2-Raytracer/run.py
@@ -1,7 +1,10 @@
 from materials import *
 from raytracing import *
+import os
 
-r = Raytracer(400, 400)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+r = Raytracer(200, 200)
 r.light=Light(position=V3(10,10,10),intensity=1.5,color=WHITE)
 
 '''
@@ -34,10 +37,42 @@ r.scene = [
     Cube(V3(-1.5, 0.75, -9.25), 0.5, skin),
 
     #mouth
-    Cube(V3(-0.5, 0.25, -9.25), 0.25, skin1),
-    Cube(V3(-0.25, 0.25, -9.25), 0.25, skin1),
-    Cube(V3(0, 0.75, -9.25), 0.25, skin1),
-    Cube(V3(0.25, 0.75, -9.25), 0.25, skin1),
+    Cube(V3(-0.6, 1.7, -9.25), 0.25, skin1),
+    Cube(V3(-0.30, 1.7, -9.25), 0.25, skin1),
+    Cube(V3(-0.10, 1.7, -9.25), 0.25, skin1),
+    Cube(V3(0, 1.7, -9.25), 0.25, skin1),
+    Cube(V3(0.15, 1.7, -9.25), 0.25, skin1),
+
+    Cube(V3(-0.5, 2.05, -9.25), 0.5, orange7),
+    Cube(V3(0, 2.05, -9.25), 0.5, orange7),
+
+    #face
+
+    Cube(V3(-0.5, 0.25, -9.25), 0.5, orange4),
+    Cube(V3(0, 0.25, -9.25), 0.5, orange4),
+
+    Cube(V3(-1.5, 1.25, -9.25), 0.5, orange4),
+    Cube(V3(1, 1.25, -9.25), 0.5, orange4),
+
+    Cube(V3(-1.5, 1.75, -9.25), 0.5, orange5),
+    Cube(V3(1, 1.75, -9.25), 0.5, orange5),
+
+    Cube(V3(-2, 1.25, -9.25), 0.5, orange5),
+    Cube(V3(-2, 1.75, -9.25), 0.5, orange6),
+    Cube(V3(-2, 2.25, -9.25), 0.5, orange3),
+    Cube(V3(-1.5, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(-1, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(-0.5, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(0, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(0.5, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(1, 2.25, -9.25), 0.5, orange6),
+    Cube(V3(1.5, 2.25, -9.25), 0.5, orange3),
+    Cube(V3(1.5, 1.75, -9.25), 0.5, orange6),
+    Cube(V3(1.5, 1.25, -9.25), 0.5, orange5),
+    
+    
+
+    
 
 ]
 
